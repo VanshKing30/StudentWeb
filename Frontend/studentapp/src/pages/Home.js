55de9c626a0d220343e6203b989d64dc4bdb5712
Vansh <vanshwaldeo360@gmail.com>
2023-10-09 22:34:26 +0530

    Update Home.js
    Made some crucial changes

--- Frontend/studentapp/src/pages/Home.js
+++ Frontend/studentapp/src/pages/Home.js
@@ -34,7 +34,9 @@ function Home() {
     
 
     return(
-        <div>
+        <div className="Parent">
+
+            
             <div className="flex justify-around">
                 <div>
                     <h1>Student Data</h1>
@@ -46,7 +48,86 @@ function Home() {
                     </Link>
                 </div>
             </div>  
+
+
+            <div className="flex flex-col mt-6">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+              <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+                  <thead className="bg-gray-50 dark:bg-gray-800">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        <span>Employee</span>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Title
+                      </th>
+
+                      <th
+                        scope="col"
+                        className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
+                      >
+                        Role
+                      </th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    {studData?.data.map((person) => (
+                      <tr key={person.name}>
+                        <td className="py-4 px-4 whitespace-nowrap">
+                          <div className="flex items-center">
+                            <div className="flex-shrink-0 h-10 w-10">
+                              {/* <img
+                                className="h-10 w-10 rounded-full object-cover"
+                                src={person.image}
+                                alt=""
+                              /> */}
+                            </div>
+                            <div className="ml-4">
+                              <div className="text-sm font-medium text-gray-900 dark:text-white">
+                                {person.name}
+                              </div>
+                              <div className="text-sm text-gray-500 dark:text-gray-300">
+                                {person.email}
+                              </div>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-12 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-900 dark:text-white">
+                            {person.title}
+                          </div>
+                          <div className="text-sm text-gray-500 dark:text-gray-300">
+                            {person.department}
+                          </div>
+                        </td>
+
+                        <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                          {person.role}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
+
+
+
+
+
+        </div>
+
 
         
 
